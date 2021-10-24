ffd7e49775043ee32843ec3b35d9a3ac4b85d93c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-10-24 09:51:02 +0530

    Update Dijkstra Algorithm(Using Priority Queue).java

--- Shortest Path Algorithms/Dijkstra Algorithm(Using Priority Queue).java
+++ Shortest Path Algorithms/Dijkstra Algorithm(Using Priority Queue).java
@@ -1,8 +1,8 @@
 class Node{
     int v,w;
     Node(int v,int w){
-       this.v=v;
-       this.w=w;
+        this.v=v;
+        this.w=w;
     }
 }
 class NodeComparator implements Comparator<Node>{
@@ -12,36 +12,33 @@ class NodeComparator implements Comparator<Node>{
 }
 class Solution
 {
-    static int[] dijkstra(int V1, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> g, int S)
     {
-        boolean visited[]=new boolean[V1];
-        int distance[]=new int[V1];
+        boolean visited[]=new boolean[V];
+        int distance[]=new int[V];
         Arrays.fill(distance,Integer.MAX_VALUE);
         distance[S]=0;
-       // PriorityQueue<Node>pq=new PriorityQueue<Node>(new NodeComparator());
-        PriorityQueue<Node>pq=new PriorityQueue<Node>((a,b)->{
-            return a.w-b.w;
-        });
+        PriorityQueue<Node>pq=new PriorityQueue<Node>(new NodeComparator());
         pq.add(new Node(S,0));
         while(!pq.isEmpty())
         {
-            Node temp=pq.poll();
-            int V=temp.v;
-            int W=temp.w;
-            visited[V]=true;
-            for(ArrayList<Integer>adjacent:adj.get(V))
-            {
-                int adjacentV=adjacent.get(0);
-                int adjacentW=adjacent.get(1);
-                if(visited[adjacentV]==false&&distance[adjacentV]>W+adjacentW)
-                {
-                    distance[adjacentV]=W+adjacentW;
-                    pq.add(new Node(adjacentV,W+adjacentW));
-                }
-            }
+           Node temp=pq.peek();
+           pq.poll();
+           int v=temp.v;
+           int w=temp.w;
+           visited[v]=true;
+           for(ArrayList<Integer>adjacent:g.get(v))
+           {
+               int adjacentV=adjacent.get(0);
+               int adjacentW=adjacent.get(1);
+               if(visited[adjacentV]==false&&distance[adjacentV]>adjacentW+w)
+               {
+                 distance[adjacentV]=adjacentW+w;
+                 pq.add(new Node(adjacentV,adjacentW+w));
+               }
+           }
         }
-        
         return distance;
-        
     }
+    
 }
